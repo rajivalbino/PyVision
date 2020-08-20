7f2d6121f18b7ef857208764df1246b8ca95d7bb
Rajiv Albino <rajivalbino@gmail.com>
2020-08-20 20:37:25 -0300

    Added contours, dilate and erode

--- ImageProcessing/improc02.py
+++ ImageProcessing/improc02.py
@@ -24,4 +24,34 @@ cv2.imshow("Thresh", thresh)
 cv2.waitKey(0)
 
 # Find Contours
-#cntrs = cv2.findContours(thresh.copy(), )
+cntrs = cv2.findContours(thresh.copy(), mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
+cntrs = imutils.grab_contours(cntrs)
+output = img.copy()
+
+for c in cntrs:
+    cv2.drawContours(output, contours=[c], contourIdx=-1, color=(240,0,159), thickness=3)
+    cv2.imshow("Contours", output)
+    cv2.waitKey(0)
+
+msg = "{} objects found!".format(len(cntrs))
+cv2.putText(output, msg, (10,25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (240,0,159), 2)
+cv2.imshow("Contours", output)
+cv2.waitKey(0)
+
+# Erosion
+mask = thresh.copy()
+mask = cv2.erode(mask, None, iterations=5)
+cv2.imshow("Erode", mask)
+cv2.waitKey(0)
+
+# Dilate
+mask = thresh.copy()
+mask = cv2.dilate(mask, None, iterations=5)
+cv2.imshow("Dilate", mask)
+cv2.waitKey(0)
+
+# Bitwise AND
+mask = cv2.cvtColor(thresh.copy(), cv2.COLOR_GRAY2BGR)
+output = cv2.bitwise_and(img, mask)
+cv2.imshow("Bitwise AND", output)
+cv2.waitKey(0)
