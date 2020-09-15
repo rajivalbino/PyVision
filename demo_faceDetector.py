d8437fa279decd44a925ab35484eb7f5cef4900e
Rajiv Albino <rajivalbino@gmail.com>
2020-09-15 00:09:46 -0300

    reorg

--- demo_faceDetector.py
+++ demo_faceDetector.py
@@ -5,27 +5,8 @@ import imutils
 import dlib
 import cv2
 from pyvision.video import WebcamStream
-
-def rect2xywh(rect):
-    # Convert dlib rect (top-left point, bottom-right point)
-    # to OpenCV rect(top-left point, width, height)
-    x = rect.left()
-    y = rect.top()
-    w = rect.right() - x
-    h = rect.bottom() - y
-
-    # return a tuple
-    return (x,y,w,h)
-
-def shape_to_np(shape, dtype="int"):
-    # initialize the list of (x, y)-coordinates
-    coords = np.zeros((68, 2), dtype=dtype)
-    # loop over the 68 facial landmarks and convert them
-    # to a 2-tuple of (x, y)-coordinates
-    for i in range(0, 68):
-        coords[i] = (shape.part(i).x, shape.part(i).y)
-    # return the list of (x, y)-coordinates
-    return coords
+from pyvision.image import rect2xywh
+from pyvision.image import shape2np
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -39,9 +20,8 @@ predictor = dlib.shape_predictor(args["shape_predictor"])
 
 # start a threaded video stream
 stream = WebcamStream(src=0).start()
-key = None
 
-while key != ord('q'):
+while True:
     frame = stream.read()
     frame = imutils.resize(frame, width=800)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -50,21 +30,22 @@ while key != ord('q'):
     rects = detector(gray,0)
 
     # for each face found
-    for (i, rect) in enumerate(rects):
+    #for (i, rect) in enumerate(rects):
+    for rect in rects:
         # bounding box of the face
         x,y,w,h = rect2xywh(rect)
         cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
 
         # facial landmarks
         shape = predictor(gray, rect)
-        shape = shape_to_np(shape)
+        shape = shape2np(shape)
 
         # draw coordinates
         for (x,y) in shape:
             cv2.circle(frame, (x,y), 2, (0,0,255), -1)
         
     cv2.imshow("Video", frame)
-    key = cv2.waitKey(1) & 0xFF
+    if cv2.waitKey(1) & 0xFF == ord('q'): break
 
 stream.stop()
 cv2.destroyAllWindows()
--- pyvision/image/__init__.py
+++ pyvision/image/__init__.py
@@ -1,1 +1,3 @@
 from .modeBlur import modeBlur
+from .dlibConvert import rect2xywh
+from .dlibConvert import shape2np
